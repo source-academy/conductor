7bc05e247424580f4c2bd82ad0014988759b75b9
tsammeow <97447335+tsammeow@users.noreply.github.com>
2024-10-15 03:30:54 +0800

    implement queue data structure

---
+++ src/common/ds/Queue.ts
@@ -0,0 +1,23 @@
+export default class Queue<T> {
+    private s1!: T[];
+    private s2!: T[];
+    constructor() {
+        this.s1 = [];
+        this.s2 = [];
+    }
+    push(item: T) {
+        this.s2.push(item);
+    }
+    pop(): T {
+        if (this.s1.length === 0) {
+            if (this.s2.length === 0) throw Error("queue is empty");
+            let temp = this.s1;
+            this.s1 = this.s2.reverse();
+            this.s2 = temp;
+        }
+        return this.s1.pop();
+    }
+    get length() {
+        return this.s1.length + this.s2.length;
+    }
+}
