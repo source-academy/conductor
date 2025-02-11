887751c1743eb697f6d132437ee3e082cc959652
tsammeow <97447335+tsammeow@users.noreply.github.com>
2025-02-12 02:15:52 +0800

    add isReferenceType utility function

---
+++ src/conductor/stdlib/util/index.ts
@@ -0,0 +1,6 @@
+export { array_assert } from "./array_assert";
+export { closure_arity_assert } from "./closure_arity_assert";
+export { closure_returntype_assert } from "./closure_returntype_assert";
+export { closure_returnvalue_checked } from "./closure_returnvalue_checked";
+export { closure_returnvalue } from "./closure_returnvalue";
+export { pair_assert } from "./pair_assert";
