91d4edff9032b64983f7268e5c6814757b65e503
tsammeow <97447335+tsammeow@users.noreply.github.com>
2025-02-13 18:03:12 +0800

    make moduleplugin keep a reference to the hooked evaluator instead of stealing all its data handling functions for itself

--- src/conductor/module/BaseModulePlugin.ts
+++ src/conductor/module/BaseModulePlugin.ts
@@ -1,18 +1,7 @@
 import { ConductorInternalError } from "../../common/errors/ConductorInternalError";
 import { IConduit, IChannel } from "../../conduit";
-import { IDataHandler, PairIdentifier, ExternValue, DataType, ArrayIdentifier, IFunctionSignature, ExternCallable, ClosureIdentifier, Identifier, OpaqueIdentifier, ReturnValue, ExternTypeOf, List } from "../types";
+import { IDataHandler } from "../types";
 import { IModulePlugin, IModuleExport } from "./types";
-
-const methods: readonly (Exclude<keyof IDataHandler, "hasDataInterface" | "stdlib">)[] = [
-    ///// Data Handling Functions
-    "pair_make", "pair_gethead", "pair_typehead", "pair_sethead", "pair_gettail", "pair_typetail", "pair_settail", "pair_assert",
-    "array_make", "array_length", "array_get", "array_type", "array_set", "array_assert",
-    "closure_make", "closure_arity", "closure_call", "closure_returnvalue", "closure_returnvalue_checked", "closure_arity_assert", "closure_returntype_assert",
-    "opaque_make", "opaque_get",
-    "tie", "untie",
-    ///// Standard library functions
-    "is_list", "accumulate", "length",
-];
 
 export abstract class BaseModulePlugin implements IModulePlugin {
     abstract readonly channelAttach: string[];
@@ -20,23 +9,19 @@ export abstract class BaseModulePlugin implements IModulePlugin {
 
     abstract exports: IModuleExport[];
 
+    evaluator!: IDataHandler;
+
     /** Is this module ready for use? */
     private __hooked: boolean = false;
 
     hook(evaluator: IDataHandler): void {
         if (this.__hooked) throw new ConductorInternalError("Module already hooked");
         this.__hooked = true;
-        for (const methodName of methods) {
-            // @ts-expect-error not forcing typing of identifiers in the evaluator is the point
-            this[methodName] = evaluator[methodName].bind(evaluator);
-        }
+        this.evaluator = evaluator;
     }
     unhook(): void {
         this.verifyHooked();
         this.__hooked = false;
-        for (const methodName of methods) {
-            delete this[methodName];
-        }
     }
     isHooked(): boolean {
         return this.__hooked;
@@ -44,39 +29,4 @@ export abstract class BaseModulePlugin implements IModulePlugin {
     verifyHooked(): void {
         if (!this.__hooked) throw new ConductorInternalError("Module not hooked");
     }
-
-    // To be populated by hook():
-    pair_make!: () => PairIdentifier;
-    pair_gethead!: (p: PairIdentifier) => ExternValue;
-    pair_typehead!: (p: PairIdentifier) => DataType;
-    pair_sethead!: (p: PairIdentifier, t: DataType, v: ExternValue) => void;
-    pair_gettail!: (p: PairIdentifier) => ExternValue;
-    pair_typetail!: (p: PairIdentifier) => DataType;
-    pair_settail!: (p: PairIdentifier, t: DataType, v: ExternValue) => void;
-    pair_assert!: (p: PairIdentifier, headType?: DataType, tailType?: DataType) => boolean;
-
-    array_make!: (t: DataType, len: number, init?: ExternValue) => ArrayIdentifier;
-    array_length!: (a: ArrayIdentifier) => number;
-    array_get!: (a: ArrayIdentifier, idx: number) => ExternValue;
-    array_type!: (a: ArrayIdentifier) => DataType;
-    array_set!: (a: ArrayIdentifier, idx: number, v: ExternValue) => void;
-    array_assert!: (a: ArrayIdentifier, type?: DataType, length?: number) => boolean;
-
-    closure_make!: <const T extends IFunctionSignature>(sig: T, func: ExternCallable<T>, dependsOn?: (Identifier | null)[]) => ClosureIdentifier<T["returnType"]>;
-    closure_arity!: (c: ClosureIdentifier<DataType>) => number;
-    closure_call!: <T extends DataType> (c: ClosureIdentifier<T>, args: ExternValue[]) => ReturnValue<NoInfer<T>>;
-    closure_returnvalue!: <T extends DataType>(rv: ReturnValue<T>) => ExternTypeOf<T>;
-    closure_returnvalue_checked!: <T extends DataType>(rv: ReturnValue<any>, type: T) => ExternTypeOf<T>;
-    closure_arity_assert!: (c: ClosureIdentifier<DataType>, arity: number) => boolean;
-    closure_returntype_assert!: <T extends DataType>(rv: ReturnValue<T>, type: T) => asserts rv is ReturnValue<T>;
-
-    opaque_make!: (v: any) => OpaqueIdentifier;
-    opaque_get!: (o: OpaqueIdentifier) => any;
-
-    tie!: (dependent: Identifier, dependee: Identifier | null) => void;
-    untie!: (dependent: Identifier, dependee: Identifier | null) => void;
-
-    is_list!: (xs: List) => boolean;
-    accumulate!: <T extends Exclude<DataType, DataType.VOID>>(resultType: T, op: ClosureIdentifier<DataType>, initial: ExternTypeOf<T>, sequence: List) => ExternTypeOf<T>;
-    length!: (xs: List) => number;
 }
