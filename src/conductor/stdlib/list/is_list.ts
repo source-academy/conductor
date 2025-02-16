ecc7f4646245ce3a39a6ccfdf39a49e81684d212
tsammeow <97447335+tsammeow@users.noreply.github.com>
2025-02-16 18:46:11 +0800

    add documentation for is_list and length stdlib functions

--- src/conductor/stdlib/list/is_list.ts
+++ src/conductor/stdlib/list/is_list.ts
@@ -1,7 +1,12 @@
 import { DataType, IDataHandler, List, PairIdentifier } from "../../types"
 
+/**
+ * Checks if a List is a true list (`tail(tail...(xs))` is empty-list).
+ * @param xs The List to check.
+ * @returns true if the provided List is a true list.
+ */
 export function is_list(this: IDataHandler, xs: List): boolean {
-    if (xs === null) return true;
+    if (xs === null) return true; // TODO: figure out some way to avoid JS value comparison
     while (true) {
         const t = this.pair_typetail(xs);
         if (t === DataType.EMPTY_LIST) return true;
--- src/conductor/stdlib/list/length.ts
+++ src/conductor/stdlib/list/length.ts
@@ -1,10 +1,15 @@
 import { DataType, IDataHandler, List, PairIdentifier } from "../../types"
 
+/**
+ * Gets the length of a List.
+ * @param xs The List to get the length of.
+ * @returns The length of the List.
+ */
 export function length(this: IDataHandler, xs: List): number {
     if (!this.is_list(xs)) {
       throw new Error('length(xs) expects a list');
     }
-    if (xs === null) return 0;
+    if (xs === null) return 0; // TODO: figure out some way to avoid JS value comparison
     let length = 1;
     while (true) {
         const t = this.pair_typetail(xs);
