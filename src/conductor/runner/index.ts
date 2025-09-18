4f72dff056f1f179fab77c509dfdfdb85f9ad02e
loyaltypollution <65063925+loyaltypollution@users.noreply.github.com>
2025-09-18 10:05:15 +0800

    Fix submodule mapping

--- src/conductor/runner/index.ts
+++ src/conductor/runner/index.ts
@@ -2,5 +2,3 @@ export * from "./types";
 export * from "./util";
 export { BasicEvaluator } from "./BasicEvaluator";
 export { RunnerPlugin } from "./RunnerPlugin";
-
-export { IRunnerPlugin, IEvaluator, IInterfacableEvaluator, EvaluatorClass } from "./types";
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-// Export common modules
-export * from "./common/errors";
-export * from "./common/util";
-
-// Export initialise method
-export { initialise } from "./conductor/runner/util";
