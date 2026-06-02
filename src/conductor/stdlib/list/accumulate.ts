c388afa00b4351516951ee9d99c753ca24c3db22
Aarav Malani <aarav.malani@gmail.com>
2026-06-02 17:05:44 +0800

    feat: switch closures to be generators

--- src/conductor/stdlib/list/accumulate.ts
+++ src/conductor/stdlib/list/accumulate.ts
@@ -11,12 +11,11 @@ import { DataType, type IDataHandler, type TypedValue } from "../../types"
  * @param resultType The (expected) type of the result.
  * @returns A Promise resolving to the result of accumulating the Closure over the List.
  */
-export async function accumulate<T extends Exclude<DataType, DataType.VOID>>(this: IDataHandler, op: TypedValue<DataType.CLOSURE, T>, initial: TypedValue<T>, sequence: TypedValue<DataType.LIST>, resultType: T): Promise<TypedValue<T>> {
+export async function* accumulate<T extends Exclude<DataType, DataType.VOID>>(this: IDataHandler, op: TypedValue<DataType.CLOSURE, T>, initial: TypedValue<T>, sequence: TypedValue<DataType.LIST>, resultType: T): AsyncGenerator<void, TypedValue<T>, undefined> {
     const vec = await this.list_to_vec(sequence);
     let result = initial;
     for (let i = vec.length - 1; i >= 0; --i) {
-        result = await this.closure_call(op, [vec[i], result], resultType);
+        result = yield* this.closure_call(op, [vec[i], result], resultType);
     }
-
     return result;
 }
