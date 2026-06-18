0d54e190c48da5fe0f0a01f871596fb7ea674b67
Shrey Jain <101659971+Shrey5132@users.noreply.github.com>
2026-06-18 23:15:12 +0800

    Update src/conductor/runner/BasicEvaluator.ts
    Co-authored-by: Akshay  <131676168+Akshay-2007-1@users.noreply.github.com>

--- src/conductor/runner/BasicEvaluator.ts
+++ src/conductor/runner/BasicEvaluator.ts
@@ -10,12 +10,6 @@ export abstract class BasicEvaluator implements IEvaluator {
             const initialChunk = await this.conductor.requestFile(entryPoint);
             if (!initialChunk) throw new ConductorInternalError("Cannot load entrypoint file");
             this.conductor.sendResult(await this.evaluateFile(entryPoint, initialChunk));
-            while (true) {
-                const chunk = await this.conductor.requestChunk();
-                this.conductor.sendResult(await this.evaluateChunk(chunk));
-            }
-            // The REPL loop only exits if the conduit is terminated externally.
-            this.conductor.updateStatus(RunnerStatus.STOPPED, true);
         } catch (e) {
             // Always notify the host so it can unblock its own REPL loop.
             this.conductor.updateStatus(RunnerStatus.ERROR, true);
